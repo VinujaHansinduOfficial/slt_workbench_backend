fe260d09ced3c4ec4565137f0c1f3733e217f1b4
VinujaHansinduOfficial <vinujahansinduofficial@gmail.com>
2025-06-26 09:58:30 +0530

    .

--- src/main/java/com/example/backend/Controller/WorkProgressController.java
+++ src/main/java/com/example/backend/Controller/WorkProgressController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @CrossOrigin(origins = "https://front-workflow-gzq3.vercel.app/")
 @RequestMapping(value = "/api/workprogress")
